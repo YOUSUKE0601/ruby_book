c1d3732bad6128f42e4e7d8f01fb62db29abc2d8
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-06-17 15:12:28 +0900

    長さ変換プログラム作成

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,42 +1,12 @@
-numbers = [1, 2, 3, 4]
-sum = 0
+def buy_burger(drink, potato)
+  if drink
+    puts "ドリンクを購入する"
+  end
 
-numbers.each do |n|
-  sum_value = n.even? ? n * 10 : n
-  sum += sum_value
-end
-
-puts sum
-
-
-new_numbers = numbers.map { |n| n * 10}
-puts new_numbers
-
-def liquid?(temperature)
-  (0...100).include?(temperature)
-end
-
-puts liquid?(-1)
-puts liquid?(0)
-puts liquid?(99)
-puts liquid?(100)
-
-def charge(age)
-  case age
-  
-  when 0..5
-     0
-  when 6..12
-     300
-  when 13..18
-     600
-  else
-     1000
+  if potato
+    puts "ポテトを購入する"
   end
 end
 
-puts charge(3)
-puts charge(12)
-puts charge(16)
-puts charge(25)
+buy_burger(drink: true, potato: true)
 
