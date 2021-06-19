91e77f9d8f9233cf7caf6c9669480c35c25588af
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-06-19 17:15:45 +0900

    ハッシュ記法を変換するプログラム完成

--- test/convert_hash_syntax_test.rb
+++ test/convert_hash_syntax_test.rb
@@ -3,6 +3,22 @@ require "./lib/convert_hash_syntax"
 
 class ConvertHashSyntaxTest < Minitest::Test
   def test_convert_hash_syntax
-    assert_equal "{}", convert_hash_syntax("{}")
+    old_syntax = <<~TEXT
+      {
+        :name => "Alice",
+        :age=>20,
+        :gender  =>  :female
+      }
+    TEXT
+    expected = <<~TEXT
+      {
+        name: "Alice",
+        age: 20,
+        gender: :female
+      }
+    TEXT
+    actual = convert_hash_syntax(old_syntax)
+    puts actual
+    assert_equal expected, actual
   end
 end
