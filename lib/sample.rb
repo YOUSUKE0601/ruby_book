9d85d68de4e6a66cfbda5b121fe8de89f89e99ae
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-06-19 16:45:56 +0900

    ハッシュ記法を変換するプログラムを作成

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,12 +1,23 @@
-def buy_burger(drink, potato)
-  if drink
-    puts "ドリンクを購入する"
-  end
+text = <<TEXT
+  私の郵便番号は1234567です。
+  僕の住所は6770056　兵庫県西脇市板波町だよ。
+TEXT
 
-  if potato
-    puts "ポテトを購入する"
-  end
-end
+puts text.gsub(/(\d{3})(\d{4})/, '\1-\2')
 
-buy_burger(drink: true, potato: true)
+text = <<TEXT
+  I love Ruby.
+  Python is a great language.
+  Java and JavaScript are different.
+TEXT
+
+puts text.scan(/[A-Z][A-Za-z]+/)
+
+text = "私の誕生日は1977年7月17日です"
+m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
+puts m[0]
+puts m[:year]
+puts m[:month]
+puts m[:day]
+
 
