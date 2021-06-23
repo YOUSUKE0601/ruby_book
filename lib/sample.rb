2705621d9e3d60ea56c213e64457b02ccbc2bb19
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-06-23 21:07:21 +0900

    不要なコード削除

--- lib/sample.rb
+++ lib/sample.rb
@@ -13,11 +13,6 @@ TEXT
 
 puts text.scan(/[A-Z][A-Za-z]+/)
 
-text = "私の誕生日は1977年7月17日です"
-m = /(?<year>\d+)年(?<month>\d+)月(?<day>\d+)日/.match(text)
-puts m[0]
-puts m[:year]
-puts m[:month]
-puts m[:day]
+
 
 
