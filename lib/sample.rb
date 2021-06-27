ced4cb224ab530bee42441abb66a1c371709a563
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-06-27 18:26:33 +0900

    コードを改良

--- lib/sample.rb
+++ lib/sample.rb
@@ -5,13 +5,7 @@ TEXT
 
 puts text.gsub(/(\d{3})(\d{4})/, '\1-\2')
 
-text = <<TEXT
-  I love Ruby.
-  Python is a great language.
-  Java and JavaScript are different.
-TEXT
 
-puts text.scan(/[A-Z][A-Za-z]+/)
 
 
 
