7487a2c56263513d492bc596b3833615d9595cf5
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-06-30 18:47:24 +0900

    [Update]改札機プログラムの作成

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,9 +1,25 @@
-text = <<TEXT
-  私の郵便番号は1234567です。
-  僕の住所は6770056　兵庫県西脇市板波町だよ。
-TEXT
+class User
+  def initialize(name)
+    @name = name
+  end
 
-puts text.gsub(/(\d{3})(\d{4})/, '\1-\2')
+  def self.create_users(names)
+    names.map do |name|
+      User.new(name)
+    end
+  end
+
+  def hello
+    "Hello, I am #{@name}."
+  end
+end
+
+names = ["Alice", "Bob", "Carol"]
+
+users = User.create_users(names)
+users.each do |user|
+  puts user.hello
+end
 
 
 
