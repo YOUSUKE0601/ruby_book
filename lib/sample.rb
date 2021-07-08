58191501c9b006c7ff164a0ba95250ed2f377caf
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-07-08 12:40:50 +0900

    [Finish]正規表現チェッカープログラム

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,40 +1,18 @@
-module Loggable
-
-  def log(text)
-    puts "[LOG] #{text}"
+def currency_of(country)
+  case country
+  when :japan
+    "yen"
+  when :us
+    "dollar"
+  when :india
+    "rupee"
+  # else
+    # raise ArgumentError, "無効な国名です。 #{country}"
   end
 end
 
-class Product
-  
-extend Loggable
-
-  
-  def self.create_products(names)
-    log 'create_products is called.'
-    'A great movie'
-  end
-
-
-end
-
-class User
-
-include Loggable
-
-  def name
-    log 'name is called.'
-    'Alice'
-  end
-
-end
-
-puts Product.create_products([])
-puts Product.log('Hello.')
-
-user = User.new
-puts user.name
-
+puts currency_of(:japan)
+puts currency_of(:italy)
 
 
 
