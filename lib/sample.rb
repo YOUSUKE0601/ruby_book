a65310124824660317a2f6dae5dca72d9e1f42ee
YOUSUKE0601 <76763700+YOUSUKE0601@users.noreply.github.com>
2021-07-10 13:27:58 +0900

    [Finish]ワードシンセサイザー

--- lib/sample.rb
+++ lib/sample.rb
@@ -1,18 +1,17 @@
-def currency_of(country)
-  case country
-  when :japan
-    "yen"
-  when :us
-    "dollar"
-  when :india
-    "rupee"
-  # else
-    # raise ArgumentError, "無効な国名です。 #{country}"
-  end
+def greeting(proc_1, proc_2, proc_3)
+  puts proc_1.call("おはよう")
+  puts proc_2.call("こんにちわ")
+  puts proc_3.call("こんばんわ")
 end
 
-puts currency_of(:japan)
-puts currency_of(:italy)
+shuffle_proc = Proc.new { |text| text.chars.shuffle.join }
+repeat_proc = Proc.new { |text| text * 2 }
+question_proc = Proc.new { |text| "#{text}?" }
+
+greeting(shuffle_proc, repeat_proc, question_proc)
+
+
+
 
 
 
